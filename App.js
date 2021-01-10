c214172bb58448e39178eaa1b73e59acc01ef03b
ricsoh <ricsoh@gmail.com>
2021-01-10 17:48:54 +0800

    Set up drawer navigation

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-//  ./App.js
+// ./App.js
 
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
-import BottomTabNavigator from "./navigation/TabNavigator";
+import DrawerNavigator from "./navigation/DrawerNavigator";
 
  const App = () => {
   return (
     <NavigationContainer>
-      <BottomTabNavigator />
+      <DrawerNavigator />
     </NavigationContainer>
   );
 }
-export default App
+export default App;
