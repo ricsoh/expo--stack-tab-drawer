6c36219e46681f35d62e302dbca9d95ea8cd204b
ricsoh <ricsoh@gmail.com>
2021-01-10 17:35:49 +0800

    Set up button on homescreen to navigate to aboutscreen

--- screens/Home.js
+++ screens/Home.js
@@ -3,11 +3,14 @@
 import React from "react";
 import { View, Button, Text, StyleSheet } from "react-native";
 
-const Home = () => {
+const Home = ({ navigation }) => {
   return (
     <View style={styles.center}>
       <Text>This is the home screen</Text>
-      <Button title="Go to About Screen" />
+      <Button
+        title="Go to About Screen"
+        onPress={() => navigation.navigate("About")} // We added an onPress event which would navigate to the About screen
+      />
     </View>
   );
 };
